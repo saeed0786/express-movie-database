eb091fd86509d43c256f5299d8e56b2b6af9bdca
Web Dev Portfolio <61039707+saeed0786@users.noreply.github.com>
2022-08-29 15:30:40 -0400

    Update index.test.js

--- index.test.js
+++ index.test.js
@@ -1,64 +1,159 @@
-const {Model} = require('./index')
-const {sequelize} = require('./db')
-const {Movie} = require('./model/movie')
-const {Crew} = require('./model/crew')
-const {Cast} = require('./model/cast')
+const {Movie, Cast, Crew, sequelize} = require('./index')
 
-
-describe('movie Database', () => {
+describe('Express Movie Database', () => {
     beforeAll(async() => {
         await sequelize.sync({force:true})
-    })
-  const arrayOfcrews =[ 
-    {crew_id: 1001, name: 'Jack Dawson', position: 'Hero', producer:'James Cameron'},
-    {crew_id: 1002, name: 'Julie Christie', position: 'Hero', producer:'David Barron'},
-    {crew_id: 1003, name: 'Harvey Keite', position: 'Hero', producer:'Lawrence Bender'}
-]
-    const arrayOfCast= [
-        {cast_id: 101, role_name:'American dreamer', gender:'male', paid_amount: 2500000},
-        {cast_id: 102, role_name:' Queen Gertrude', gender:'Female', paid_amount: 200000},
-        {cast_id: 103, role_name:' tough guy', gender:'male', paid_amount: 150000}
+
+        //create array of movies that will be added to the Movie model
+        const arrayOfMovies = [
+            {title: 'Pearl Harbor'},
+            {title: 'Home Alone 1'},
+            {title: 'Hunter Games'},
+            {title: 'Black Hawk Down'}
+        ]
+
+        //create array of casts that will be added to the Cast model
+        const arrayOfCasts = [
+            {name: "Ben Affleck", role: "Captain Rafe McCawley"},
+            {name: "Josh Hartnet", role: "Captain Danny Walker"},
+            {name: "Kate Beckinsale", role: "Evelyn Johnson"}
+        ]
+
+        //create array of crews that will be added to the Crew model
+        const arrayOfCrews = [
+            {name: "John Schwartzman", position: "Director of Photography", department: "Cinematography"},
+            {name: "Hans Zimmer", position: "Music", department: "Music"},
+            {name: "Nigel Phelps", position: "Production Design", department: "Production Design"},
+            {name: "Martin Laing", position: "Supervising Art Director", department: "Art Direction"}
+        ]
+
+        //added arrays to the db
+        await Movie.bulkCreate(arrayOfMovies)
+        await Cast.bulkCreate(arrayOfCasts)
+        await Crew.bulkCreate(arrayOfCrews)
         
-]
-const arrayOfmovies=[
-    {movie_id:1, movie_title:'Titanic'},
-    {movie_id:2, movie_title: 'Hamlet'},
-    {movie_id:3, movie_title:'Reservoir Dogs'}
-]
-  
-test('Movie has movies', async() => {
-    await Movie.bulkCreate(arrayOfmovies)
-       const testmovie = await Movie.findOne({
-        where: {
-            movie_title: 'Titanic'
-        }
-      });
-      expect(testmovie.movie_title).toBe('Titanic')
     })
 
+    //test cases for Movie model
+    test('can create movie', async() => {
+        //read movie instance from db
+        const movieList = await Movie.findAll()
 
-    test('Crew has name ', async() => {
-        await Crew.bulkCreate(arrayOfcrews)
-           const testcrew= await Crew.findOne({
+        //assert that lenght of movieList is 4
+        expect(movieList.length).toBe(4)
+    })
+
+    test('movie has title', async() => {
+        //read movie instance from db
+        const movieName = await Movie.findAll()
+
+        //assert that movie title is Pearl Harbor
+        expect(movieName[0].title).toBe('Pearl Harbor')
+    })
+
+    //test cases for Cast model
+    test('can create cast', async() => {
+        //read cast instance from db
+        const castList = await Cast.findAll()
+
+        //assert that length of castList is 3
+        expect(castList.length).toBe(3)
+    })
+
+    test('cast has name', async() => {
+        //read cast instance from db
+        const castName = await Cast.findAll()
+
+        //assert that name of cast index 1 is Josh Hartnet
+        expect(castName[1].name).toBe('Josh Hartnet')
+    })
+
+    test('cast has role', async() => {
+        //read cast instance from db
+        const castRole = await Cast.findAll()
+
+        //assert that role of cast index 1 is Captain Danny Walker
+        expect(castRole[1].role).toBe('Captain Danny Walker')
+    })
+
+    test('movie has casts', async() => {
+        //read movie instance from db
+        const movieTitle = await Movie.findOne({
             where: {
-                name: 'Jack Dawson'
+                title: "Pearl Harbor"
             }
-          });
-          expect(testcrew.name).toBe('Jack Dawson')
-        })
+        });
 
+        //read cast instance from db
+        const castList = await Cast.findAll()
 
-        test('Cast paid for the movie', async() => {
-            await Cast.bulkCreate(arrayOfCast)
-               const testcast= await Cast.findOne({
-                where: {
-                    paid_amount: 2500000
-                }
-              });
-              expect(testcast.paid_amount).toBe(2500000)
-            })
-afterAll(async()=> {
-    // await sequelize.sync({force:true})
-    sequelize.close()
+        //add all casts to movie
+        await movieTitle.addCast(castList)
+
+        //retrieve list of casts in this movie
+        const allCast = await movieTitle.getCasts()
+
+        //assert that lenght is 3
+        expect(allCast.length).toBe(3)
+    })
+
+    //test cases for Crew model
+    test('crew has name', async() => {
+        //read crew instance from db 
+        const crewName = await Crew.findAll()
+
+        //assert that name of crew index 1 is 
+        expect(crewName[1].name).toBe('Hans Zimmer')
+    })
+
+    test('can create crew', async() => {
+        //read crew instance from db
+        const crewList = await Crew.findAll()
+
+        //assert that crewList lenght is 4
+        expect(crewList.length).toBe(4)
+    })
+
+    test('crew has position', async() => {
+        //read crew instance from db 
+        const crewPosition = await Crew.findAll()
+
+        //assert that position of crewPosition index 2
+        expect(crewPosition[2].position).toBe('Production Design')
+    })
+
+    test('crew has department', async() => {
+        //read crew instance from db
+        const crewDepartment = await Crew.findAll()
+
+        //assert that department of crewDepartment index 0
+        expect(crewDepartment[0].department).toBe('Cinematography')
+    })
+
+    test('movie has crews', async() => {
+        //read movie instance from db
+        const movieTitle = await Movie.findOne({
+            where: {
+                title: "Pearl Harbor"
+            }
+        });
+
+        //read crews instance from db
+        const crewLists = await Crew.findAll()
+
+        //add all crews to movie
+        await movieTitle.addCrew(crewLists)
+
+        //retrieve list of casts in this movie
+        const allCrew = await movieTitle.getCrews()
+
+        //assert that lenght is 3
+        expect(allCrew.length).toBe(4)
+    })
+
+    afterAll(async()=> {
+        sequelize.close()
+    })
+
 })
-})
+
